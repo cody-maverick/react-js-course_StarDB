048d305826a8c2205d1aabf1098ecd34d85f8c91
Alexander Matveev <rocknroll8905@gmail.com>
2019-01-19 17:32:04 +0300

    updatePlanet

--- src/index.js
+++ src/index.js
@@ -1,55 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-class SwapiService {
-
-  _apiBase = 'https://swapi.co/api';
-
-  async getResourse(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}` + `, received ${res.status}`);
-    }
-
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResourse(`/people/`);
-    return res.results;
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResourse(`/planets/`);
-    return res.results;
-  }
-
-  async getAllStarships() {
-    const res = await this.getResourse(`/starships/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResourse(`/people/${id}/`)
-  }
-
-  getPlanet(id) {
-    return this.getResourse(`/people/${id}/`)
-  }
-
-  getAllStarships(id) {
-    return this.getResourse(`/people/${id}/`)
-  }
-}
-
-
-const swapi = new SwapiService();
-
-swapi.getPerson(3).then((people) => {
-  console.log(people.name);
-
-
-})
-
-
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
