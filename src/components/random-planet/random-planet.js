76289f3fd0acd906cbf4ca71a2704c063cddefd2
Alexander Matveev <rocknroll8905@gmail.com>
2019-01-22 12:43:36 +0300

    spinner not done

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -3,17 +3,14 @@ import React, { Component } from 'react';
 import SwapiService from '../../services/swapi';
 
 import './random-planet.css';
+import Spinner from '../spinner';
 
 export default class RandomPlanet extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
+    planet: {}
   }
 
   constructor() {
@@ -21,23 +18,23 @@ export default class RandomPlanet extends Component {
     this.updatePlanet();
   }
 
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  }
+
   updatePlanet() {
     const id = Math.floor(Math.random() * 25 + 2);
-    this.swapiService.getPlanet(id)
-      .then((planet) => {
-        this.setState({
-          id,
-          name: planet.name,
-          population: planet.population,
-          rotationPeriod: planet.rotation_period,
-          diameter: planet.diameter
-        })
-      })
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
   }
 
   render() {
 
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const { planet: { id, name, population, rotationPeriod, diameter }
+    } = this.state;
+
+    return <Spinner/>
 
     return (
       <div className="random-planet jumbotron rounded">
@@ -61,7 +58,6 @@ export default class RandomPlanet extends Component {
           </ul>
         </div>
       </div>
-
     );
   }
 }
